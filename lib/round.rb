93423ec2c6aec163e87a34f49d3723b49da1690e
Yoseph Anderson <yrganderson@gmail.com>
2016-05-19 08:41:26 -0600

    Final Project

--- lib/round.rb
+++ lib/round.rb
@@ -28,18 +28,30 @@ class Round
     "#{percentage}%"
   end
 
-  def start
-    card_count = 0
-    puts "Welcome! You're playing with #{deck.cards.count} cards."
-    puts "-----------------------------------------"
-    until card_count == deck.cards.count
-      puts "This card is #{@guesses.length} out of #{deck.cards.count}."
-      puts "Question: #{current_card.question}"
-      record_guess(gets.chomp)
-      puts "#{guesses[card_count].feedback}"
-      card_count += 1
-    end
+  def end_game_text
     puts "****** Game over! ******"
     puts "You had #{number_correct} correct guesses out of #{deck.cards.count} for a score of #{percent_correct}."
+  end
+
+  def start_game_text
+    puts "Welcome! You're playing with #{deck.cards.count} cards."
+    puts "-----------------------------------------"
+  end
+
+  def display_question
+    puts "This card is #{@guesses.length + 1} out of #{deck.cards.count}."
+    puts "Question: #{current_card.question}"
+  end
+
+  def start
+    card_index = 0
+    start_game_text
+    until card_index == deck.cards.count
+      display_question
+      record_guess(gets.chomp)
+      puts "#{guesses[card_index].feedback}"
+      card_index += 1
+    end
+    end_game_text
   end
 end
